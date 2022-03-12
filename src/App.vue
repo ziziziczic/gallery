6c67c6632c6d69147dfa62e3b5b383af266c2691
hometown <ziziziczic@gmail.com>
2022-03-12 16:44:35 +0900

    add item

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
+<header>
   <div class="menu-container">
-    <div v-for="menu in menus" :key="menu" class="menu">
+    <h1 v-for="menu in menus" :key="menu" class="menu">
         {{menu}}
+    </h1>
+  </div>
+</header>
+<section>
+  <div class="items-container">
+    <div v-for="item in data" :key="item.id">
+      <h1>title : {{item.title}}</h1>
+      <p>price : {{item.price}}</p>
     </div>
   </div>
+</section>
 
-  <Item v-for="item in data" :key="item.title" :item="item">
+
+  <!-- <Item v-for="item in data" :key="item.title" :item="item"> -->
     <!-- <Item :item="item"/> -->
-  </Item>
+  <!-- </Item> -->
 </template>
 
 <script>
 import data from './assets/data';
-import Item from './components/item';
+// import Item from './components/item';
 
 export default {
   name: 'App',
@@ -23,7 +34,7 @@ export default {
     }
   },
   components: {
-    Item,
+    // Item,
   },
 }
 </script>
@@ -31,8 +42,17 @@ export default {
 <style>
   * {padding:0; margin:0;box-sizing: border-box;}
   ul,li{list-style: none;}
-  .menu-container{display:flex; width:900px; justify-content: space-around;align-items: center;background: darkmagenta; margin:0 auto;border-radius: 5px;}
-  .menu-container div {padding:10px;color:#fff;font-weight: 700;}
-  .menu-container div:hover {cursor: pointer; color:yellow;}
+  header,section{width:900px;margin:0 auto;}
+  .menu-container{display:flex;width:100%;justify-content:space-around;align-items: center;background: darkmagenta;border-radius: 5px;}
+  .menu-container h1 {padding:20px;color:#fff;font-weight: 700;}
+  .menu-container h1:hover {cursor: pointer; color:yellow;}
+
+  .items-container {width:100%;display:flex;justify-content: space-around;}
+  .items-container div {width:100%; flex: 1 1 30%;border:2px solid #eee;margin:5px; display:flex;flex-direction: column;cursor: pointer;}
+  .items-container div:first-child {margin-left:0px;}
+  .items-container div:nth-child(3n) {margin-right:0px;}
+  .items-container div:hover h1{color: red;}
+  .items-container h1 {color:darkmagenta;font-weight: 700px; padding:20px; text-align: center;}
+  .items-container p {color:#000; padding: 20px;text-align: center;}
 
 </style>
