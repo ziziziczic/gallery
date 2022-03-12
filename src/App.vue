5a4df019df7a4721d0c8e4300d17d1e81d8156f4
hometown <ziziziczic@gmail.com>
2022-03-12 17:47:18 +0900

    add modal window

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,29 @@
 </header>
 <section>
   <div class="items-container">
-    <div v-for="item in data" :key="item.id">
-      <h1>title : {{item.title}}</h1>
+    <div v-for="(item,index) in data" :key="item.id">
+      <h1 @click="modalopen=true; selectedNum=index;">title : {{item.title}}</h1>
       <p>price : {{item.price}}</p>
     </div>
   </div>
 </section>
 
+<div class="modal" v-if="modalopen">
+  <div class="img-container">
+    <img :src="data[selectedNum].url" :alt="data[selectedNum].title">
+  </div>
+  <div class="cont-container">
+    <h1>title : {{data[selectedNum].title}}</h1>
+    <div class="profil">
+      <p>author : {{data[selectedNum].author}}</p>
+      <div>
+        <img :src="data[selectedNum].picture" :alt="data[selectedNum].title"/>
+      </div>
+    </div>
+    <p>price : {{data[selectedNum].price}}</p>
+    <button @click="modalopen=false">close</button>
+  </div>
+</div>
 
   <!-- <Item v-for="item in data" :key="item.title" :item="item"> -->
     <!-- <Item :item="item"/> -->
@@ -31,6 +47,8 @@ export default {
     return {
       menus : ['home','shop','about'],
       data,
+      selectedNum : 0,
+      modalopen : false,
     }
   },
   components: {
@@ -54,5 +72,14 @@ export default {
   .items-container div:hover h1{color: red;}
   .items-container h1 {color:darkmagenta;font-weight: 700px; padding:20px; text-align: center;}
   .items-container p {color:#000; padding: 20px;text-align: center;}
+
+  .modal {position:fixed;top:0;left:50%;transform:translateX(-50%);width:900px; background:#ddd;padding:10px;border-radius: 5px; display: flex;}
+  .img-container{flex-basis: 50%;}
+  .img-container img{width:100%;height:auto;}
+  .cont-container{flex-basis: 50%;padding:10px;display:flex;flex-direction: column;justify-content: space-evenly;align-items: center;}
+  .cont-container .profil{display: flex; width:100%;justify-content: space-evenly;align-items: center;}
+  .cont-container .profil div{width:50%;}
+  .cont-container .profil div img{width:150px; height:150px;object-fit: contain;}
+  .cont-container button {font-size: 1rem;background: #fff;outline: none;padding:10px;}
 
 </style>
